a2710eaa26d10c2d198383dcfa68a4aa43eb9852
thiagootuler <thiagootuler@hotmail.com>
2020-07-12 23:01:02 -0300

    - Refatoração do template para Jinja - Estruturação inicial do projeto em Single Module

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  return render_template('index.html')
+
+@app.route("/publicacoes")
+def publicacoes():
+  return render_template('publicacoes.html')
+
+@app.route("/membros")
+def membros():
+  return render_template('membros.html')
+
+@app.route("/contato")
+def contato():
+  return render_template('contato.html')
+
+if __name__ == "__main__":
+  app.run()
