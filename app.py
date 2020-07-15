1859f32c1a896a1497fb3cf56787ac0335e3f8f3
thiagootuler <thiagootuler@hotmail.com>
2020-07-14 21:33:42 -0300

    - Refatoração do template para Jinja parte 2

--- app.py
+++ app.py
@@ -4,19 +4,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
 @app.route("/publicacoes")
 def publicacoes():
-  return render_template('publicacoes.html')
+    return render_template('publicacoes.html')
+
+@app.route("/artigo")
+def artigo():
+    return render_template('artigo.html')
 
 @app.route("/membros")
 def membros():
-  return render_template('membros.html')
+    return render_template('membros.html')
 
 @app.route("/contato")
 def contato():
-  return render_template('contato.html')
+    return render_template('contato.html')
 
 if __name__ == "__main__":
-  app.run()
+  app.run(debug=True)
