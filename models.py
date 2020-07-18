1b6d7bb089b8b3e105a064c5164e09a0d5cee15b
thiagootuler <thiagootuler@hotmail.com>
2020-07-18 19:02:38 -0300

    Criação das classes relacionadas com Contato

--- models.py
+++ models.py
@@ -71,6 +71,94 @@ class Membro:
         return self._resumo
 
 
+class Localizacao:
+    def __init__(self, logradouro, numero, bairro, cidade, uf):
+        self._logradouro = logradouro
+        self._numero = numero
+        self._bairro = bairro
+        self._cidade = cidade
+        self._uf = uf
+
+    def __str__(self):
+        return "{}, {} - {}, {}/{}".format(
+            self._logradouro,
+            self._numero,
+            self._bairro,
+            self._cidade,
+            self._uf
+        )
+
+
+class Coordenada:
+    def __init__(self, longitude, latitude, altitude):
+        self._longitude = str(longitude)
+        self._latitude = str(latitude)
+        self._altitude = str(altitude)
+
+    def __str__(self):
+        return ','.join(self._longitude, self._latitude, self._altitude)
+
+
+class Endereco:
+    def __init__(self, nome, instituicao, departamento, localizacao, cep, coordenada):
+        self._nome = nome
+        self._instituicao = instituicao
+        self._departamento = departamento
+        self._localizacao = localizacao
+        self._cep = cep
+        self._coordenada = coordenada
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @property
+    def instituicao(self):
+        return self._instituicao
+
+    @property
+    def departamento(self):
+        return self._departamento
+
+    @property
+    def localizacao(self):
+        return str(self._localizacao)
+
+    @property
+    def cep(self):
+        cep = self._cep
+        return "%s.%s - %s" % (cep[0:2], cep[2:5], cep[5:8])
+
+    @property
+    def coordenada(self):
+        return str(self._coordenada)
+
+
+class Contato:
+    def __init__(self, endereco, telefone, email, mapa):
+        self._endereco = endereco
+        self._telefone = telefone
+        self._email = email
+        self._mapa = mapa
+
+    @property
+    def endereco(self):
+        return self._endereco
+
+    @property
+    def telefone(self):
+        tel = self._telefone
+        return "(%s) %s - %s" % (tel[0:2], tel[2:6], tel[6:10])
+
+    @property
+    def email(self):
+        return self._email
+
+    @property
+    def mapa(self):
+        return self._mapa
+
+
 class CarregaDados():
     def __init__(self):
         self.apresentacao = """
@@ -107,16 +195,16 @@ class CarregaDados():
             resumo="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam consequatur autem rerum nam quaerat placeat cupiditate, atque enim expedita obcaecati, quae fugit tempora illum facilis neque id laboriosam, dolores sapiente."
         )
         self.membros = [membro for i in range(7)]
-        self.contato = {
-            'endereco': [
-                "NOME Research Group",
-                "Universidade Federal do Espírito Santo(UFES) - Campus de Alegre",
-                "Departamento de Ciências Florestais e da Madeira(DCFM)",
-                "Av. Gov. Lindenberg, 316 - Centro, Jerônimo Monteiro/ES",
-                "29.550-000",
-                [-20.7898091, -41.3897484, 19]
-            ],
-            'telefone': '(00) 0000 - 0000',
-            'email': 'contato@mail.dominio.com',
-            'mapa': "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1865.0067791136726!2d-41.38998983161366!3d-20.790739381509162!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xbbe71d08ccb731%3A0x43f2a4d6bcdc9cb3!2sDepartamento+de+Ci%C3%AAncias+Florestais+e+da+Madeira+-+CCAE%2FUFES!5e0!3m2!1spt-BR!2sbr!4v1556632159367!5m2!1spt-BR!2sbr"
-        }
+        self.contato = Contato(
+            endereco = Endereco(
+                nome="NOME Research Group",
+                instituicao="Universidade Federal do Espírito Santo (UFES) - Campus de Alegre",
+                departamento="Departamento de Ciências Florestais e da Madeira (DCFM)",
+                localizacao=Localizacao("Av. Gov. Lindenberg", "316", "Centro", "Jerônimo Monteiro", "ES"),
+                cep="29550000",
+                coordenada=Coordenada(-20.7898091, -41.3897484, 19)
+            ),
+            telefone='0000000000',
+            email="contato@mail.dominio.com",
+            mapa="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1865.0067791136726!2d-41.38998983161366!3d-20.790739381509162!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xbbe71d08ccb731%3A0x43f2a4d6bcdc9cb3!2sDepartamento+de+Ci%C3%AAncias+Florestais+e+da+Madeira+-+CCAE%2FUFES!5e0!3m2!1spt-BR!2sbr!4v1556632159367!5m2!1spt-BR!2sbr"
+        )
