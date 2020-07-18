09b74c6a01a0b8dee7f984dc12a878cb1f88727f
thiagootuler <thiagootuler@hotmail.com>
2020-07-18 18:46:34 -0300

    Classe para carga dos dados de teste

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, url_for
-from models import apresentacao, artigo, membro, contato
+from models import CarregaDados
 from datetime import datetime
+
+dados = CarregaDados()
 
 app = Flask(__name__)
 
 @app.context_processor
-def inject_variables(contato=contato):
+def inject_variables(contato=dados.contato):
     return dict(
         header_title="<strong>NOME</strong> Research Group",
         footer_contact=contato,
@@ -13,23 +15,23 @@ def inject_variables(contato=contato):
     )
 
 @app.route("/")
-def index(apresentacao=apresentacao):
+def index(apresentacao=dados.apresentacao):
     return render_template('index.html', conteudo=apresentacao)
 
 @app.route("/publicacoes")
-def publicacoes(publicacoes=[artigo]*2):
+def publicacoes(publicacoes=dados.publicacoes):
     return render_template('publicacoes.html', titulo="Publicações", conteudo=publicacoes)
 
 @app.route("/artigo")
-def artigo(artigo=artigo):
+def artigo(artigo=dados.artigo):
     return render_template('artigo.html', conteudo=artigo)
 
 @app.route("/membros")
-def membros(membros=[membro for i in range(7)]):
+def membros(membros=dados.membros):
     return render_template('membros.html', titulo="Equipe de pesquisadores", conteudo=membros)
 
 @app.route("/contato")
-def contato(contato=contato):
+def contato(contato=dados.contato):
     return render_template('contato.html', titulo="Formas de contato", conteudo=contato)
 
 if __name__ == "__main__":
